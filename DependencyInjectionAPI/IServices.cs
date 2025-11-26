a6ce6b513e8018d949eaba585359bdf5504ad54b
Akshaya <akshaya@astravor.com>
2025-11-26 22:59:25 +0530

    ADDED: DependencyInjection files

---
+++ DependencyInjectionAPI/IServices.cs
@@ -0,0 +1,19 @@
+﻿namespace DependencyInjectionAPI;
+
+public interface IServices
+{
+    public interface ITransientService
+    {
+        Guid Id { get; }
+    }
+
+    public interface IScopedService
+    {
+        Guid Id { get; }
+    }
+
+    public interface ISingletonService
+    {
+        Guid Id { get; }
+    }
+}
---
+++ DependencyInjectionAPI/Model/Services.cs
@@ -0,0 +1,16 @@
+﻿namespace DependencyInjectionAPI
+{
+    public class Services
+    {
+        public interface IMyService
+        {
+            Guid Id { get; }
+        }
+
+        public class MyService : IMyService
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+
+    }
+}
